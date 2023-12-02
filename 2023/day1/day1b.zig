1b9e7440fcd627326235ef8c67449579d077de0a
Dan Mack <mack@macktronics.com>
2023-12-01 18:34:06 -0600

    Completed 2023 day1/part1

---
+++ 2023/day1/day1b.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() !void {
+    const fileName = "input.txt";
+    const file = try std.fs.cwd().openFile(fileName, .{});
+    defer file.close();
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+    const read_buf = try file.readToEndAlloc(allocator, 1024*1024);
+    defer allocator.free(read_buf);
+
+    var sum: u32 = 0;
+
+    var iter = std.mem.split(u8, read_buf, "\n");
+    while (iter.next()) |code| {
+        if (code.len > 0) {
+            var first: u8 = 0;
+            var last: u8 = 0;
+            var seen_first = false;
+            for (code) |dig| {
+                if (dig > 47 and dig < 58) {
+                    if (seen_first) {
+                        last = dig - 48;
+                    } else {
+                        seen_first = true;
+                        first = dig - 48;
+                        last = first;
+                    }
+                }
+
+            }
+            sum += (first * 10) + last;
+            print("[{}{}]\n", .{first, last});
+        }
+    }
+    print("Sum is {}\n", .{sum});
+}
